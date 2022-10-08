194efa1e4222a6a0f915e3eed1d2937cf3572dcd
Alessandro Siqueira <sickeira@gmail.com>
2022-10-08 00:25:52 -0300

    Create 02helloworld.ino

---
+++ 02helloworld.ino
@@ -0,0 +1,15 @@
+#include <DigiKeyboard.h> 
+
+void setup() {
+  // código aqui para rodar uma vez:
+  DigiKeyboard.delay (500);  
+    DigiKeyboard.println ("Exemplo de teste HELLO WORLD para a Campus Party 2022");
+  DigiKeyboard.delay (500);
+}
+
+void loop() {
+  // código principal aqui, para rodar em loop:
+  DigiKeyboard.delay (500);
+    DigiKeyboard.println ("Obrigado por comparecer na bancada Dumont Hackerspace da Campus Party SP 2022");
+  DigiKeyboard.delay (500);
+}
